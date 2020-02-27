fcb9e067c8931e302bd67c7de08fa54a450671a1
sporontinno <temtemtheone@gmail.com>
2020-02-26 19:48:00 -0800

    html structuring and css designing

---
+++ practice/javascript/functions.js
@@ -0,0 +1,14 @@
+// intro to Functions
+
+// Example of functions we have used so far.
+
+console.log();
+Math.round();
+Math.floor();
+Math.ceil();
+Math.random();
+
+// A function that belongs to a class ("library") is called a "Method".
+// A variable that belongs to a class ("library") is called a "Property"
+
+var currentTime = date.now();
