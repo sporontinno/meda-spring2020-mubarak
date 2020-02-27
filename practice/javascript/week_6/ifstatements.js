c0c137c27698863b40d9dbe46abbde52c4db20f8
sporontinno <temtemtheone@gmail.com>
2020-02-27 12:52:29 -0800

    into to 'if statements'

---
+++ practice/javascript/week_6/ifstatements.js
@@ -0,0 +1,36 @@
+// If-Else Statements, conditional statements
+
+// if (condition) {}
+
+if (true){
+
+    console.log("This code ran if the statement was true");
+
+}
+
+console.log("End of script")
+
+// Comparison operators or Comparitors
+// don't use comparitors for "strings"
+
+/*
+== equal to
+>= greater than or equal to
+> greater than
+<= lesser than or equal to
+< lesser than
+!= not equal to (should not equal to, to be true)
+*/
+
+var age = 22;
+
+if (age >= 21) {
+    console.log("Congratz, go get drunk");
+}
+
+
+var password = "ad45";
+
+if (password=="ad45") {
+    console.log("welcome");
+}
