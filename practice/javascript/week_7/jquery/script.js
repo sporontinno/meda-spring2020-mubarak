e568962e66c0b421e363527a2267a0967fea06a0
sporontinno <temtemtheone@gmail.com>
2020-03-09 12:39:48 -0700

    more jQuery practicing

--- practice/javascript/week_7/jquery/script.js
+++ practice/javascript/week_7/jquery/script.js
@@ -3,44 +3,33 @@
 
 jQuery(document).ready( function() {
 
-    $("#light-off").click(function () {
-        $("body").css({"background-color": "black", "color": "white"});
-    });
+    $("body").empty();
 
-    $("#light-on").click(function () {
-
-        var body = $("body");
-        body.css({"background-color": "white"});
-        body.css("color", "black");
-    });
-
-    // new javascript function, takes two arguments, first is callback, second is how long in milliseconds.
-    setTimeout(function () {
-
-    // Add an element to the webpage. it adds it to the end.
-    $("ol").append("<li>This was added after the fact.</li>");
-
-    $("body").append("<h1>New section</h1><p>Welcome to this new section</p>");
-
-    // Adds an element to the start of the child list, .prepend()
-    $("body").prepend("<h1 style='color: red'>Alert! new virus!</h1>");
-
-    // Adds an html as a child, replacing existing children, . html()
-    $("ol").html("<li>All tasks done!</li>");
+    var body = $("body");
     
+    body.append("<h1>Welcome</h1>");
+    $("h1").css("border", "10px dashed black");
 
-}, 3000);
+    body.append("<p>Topic One</p>");
 
-$("#recycleButton").click(function() {
-    
-    // Removes the element you have selected.
-    $('body').remove();
-    
-    // Removes all the children of the element you have selected.
-    $('body').empty();
+    body.append("<ul></ul>");
 
-})
-    
+    $("ul").append("<li>Item 1</li>");
+    $("ul").append("<li>Item 2</li>");
+    $("ul").append("<li>Item 3</li>");
 
-} );
+    $("ul").css("background-color", "lightgrey");
+
+    $("li:nth-child(2)").remove();
+
+    $("li")[1].remove();
+
+
+    // jQuery .next(), .prev()
+    // JQuery .parent(), .children()
+
+    $("body").children("ul").prev().prev().next();
+
+});
+
 
